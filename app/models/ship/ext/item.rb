f6b2c1de23d5e55c4fc5cd19f07020de370ac821
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-05 16:21:53 +0800

    ship loigc

--- app/models/ship/ext/item.rb
+++ app/models/ship/ext/item.rb
@@ -3,7 +3,7 @@ module Ship
     extend ActiveSupport::Concern
 
     included do
-      has_many :packageds, class_name: 'Ship::Packaged', foreign_key: :trade_item_id, dependent: :destroy_async
+      has_many :packageds, class_name: 'Ship::Packaged', dependent: :destroy_async
       has_many :packages, class_name: 'Ship::Package', through: :packageds
 
       belongs_to :box_hold, ->(o) { where(organ_id: o.organ_id, member_id: o.member_id, box_specification_id: o.good.box_specification_id) }, class_name: 'Ship::BoxHold', foreign_key: :user_id, primary_key: :user_id, optional: true
