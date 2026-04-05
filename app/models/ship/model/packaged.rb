b9901bea1afdcbd343c627c2617595cee2073847
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-05 13:02:07 +0800

    packaged

--- app/models/ship/model/packaged.rb
+++ app/models/ship/model/packaged.rb
@@ -3,37 +3,37 @@ module Ship
     extend ActiveSupport::Concern
 
     included do
-      attribute :trade_item_status, :string
+      attribute :item_status, :string
 
-      belongs_to :trade_item, class_name: 'Trade::Item'
-      belongs_to :good_item, optional: true, polymorphic: true
+      belongs_to :item, class_name: 'Trade::Item'
+      belongs_to :good, optional: true, polymorphic: true
       belongs_to :package, inverse_of: :packageds, counter_cache: true
 
-      before_validation :compute_trade_item, if: -> { good_item_id.present? }
+      before_validation :compute_item, if: -> { good_id.present? }
       after_create :update_status
-      after_save :sync_trade_item_status, if: -> { saved_change_to_trade_item_id? }
+      after_save :sync_item_status, if: -> { saved_change_to_item_id? }
       after_destroy :revert_status
     end
 
-    def compute_trade_item
-      if good_item_type == 'Factory::ProductionItem'
-        self.trade_item = (package.address || package.user).items.packable.find_by(good_type: 'Factory::Production', good_id: good_item.production_id)
-      elsif good_item_type == 'Ship::Box'
-        self.trade_item = (package.address || package.user).items.packable.find_by(good_type: 'Ship::BoxHost', good_id: good_item.box_host.id)
+    def compute_item
+      if good_type == 'Factory::ProductionItem'
+        self.item = (package.address || package.user).items.packable.find_by(good_type: 'Factory::Production', good_id: good.production_id)
+      elsif good_type == 'Ship::Box'
+        self.item = (package.address || package.user).items.packable.find_by(good_type: 'Ship::BoxHost', good_id: good.box_host.id)
       end
     end
 
     def update_status
-      self.trade_item.update status: 'packaged'
+      self.item.update status: 'packaged'
     end
 
-    def sync_trade_item_status
-      self.trade_item_status = trade_item.status
+    def sync_item_status
+      self.item_status = item.status
       self.save
     end
 
     def revert_status
-      self.trade_item.update status: self.trade_item_status
+      self.item.update status: self.item_status
     end
 
   end
