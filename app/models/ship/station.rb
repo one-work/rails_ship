cd52473b5eb19ecfcb05896bfa6747bb0e872637
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-27 12:09:12 +0800

    fix

--- app/models/ship/station.rb
+++ app/models/ship/station.rb
@@ -2,6 +2,6 @@ module Ship
   class Station < ApplicationRecord
     self.table_name = 'space_stations'
     include Model::Station
-    include Space::Model::Station
+    include Space::Model::Station if defined? RailsSpace
   end
 end
