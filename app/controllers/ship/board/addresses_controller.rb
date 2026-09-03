41d7e72665adaa931a74b7bd12872f6a9c8251aa
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-03 13:46:54 +0800

    addres

--- app/controllers/ship/board/addresses_controller.rb
+++ app/controllers/ship/board/addresses_controller.rb
@@ -1,6 +1,14 @@
 module Ship
   class Board::AddressesController < My::AddressesController
 
+    def index
+      q_params = {}
+      q_params.merge! organ_id: nil
+
+      @addresses = current_user.addresses.includes(:area, :station).where(q_params).order(id: :desc).page(params[:page])
+      @address = current_user.addresses.build
+    end
+
     private
     def address_params
       super.merge! organ_id: nil
